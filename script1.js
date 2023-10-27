9e51c7feaac2e347a159a1586aab317d7af3f1fd
Deba2608 <146311899+Deba2608@users.noreply.github.com>
2023-10-27 21:32:22 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -3,7 +3,8 @@ const searchForm = document.querySelector(".search-form");
 
 searchIcon.addEventListener("click", () => {
     searchForm.classList.add("active"); 
-    cartItemsContainer.classList.remove("active");  
+    cartItemsContainer.classList.remove("active"); 
+    navbar.classList.remove("active");
 });
 
 const cartIcon = document.querySelector(".cart-icon");
@@ -11,5 +12,20 @@ const cartItemsContainer = document.querySelector(".cart-items-container");
 
 cartIcon.addEventListener("click", () => {
     cartItemsContainer.classList.add("active"); 
-    searchForm.classList.remove("active");  
+    searchForm.classList.remove("active"); 
+    navbar.classList.remove("active");
 });
+const menuIcon = document.querySelector(".menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+    navbar.classList.add("active");
+    searchForm.classList.remove("active");
+    cartItemsContainer.classList.remove("active");
+});
+
+window.onscroll = () =>{
+    navbar.classList.remove("active");
+    searchForm.classList.remove("active");
+    cartItemsContainer.classList.remove("active");
+}
