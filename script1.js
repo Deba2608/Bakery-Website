d3237ef51c2b7f862218557225163472c9fd03e9
Debayan Roy <146311899+Deba2608@users.noreply.github.com>
2024-02-15 00:27:54 +0530

    Update script1.js
    eventListner

--- script1.js
+++ script1.js
@@ -2,30 +2,46 @@ const searchIcon = document.querySelector(".search-icon");
 const searchForm = document.querySelector(".search-form");
 
 searchIcon.addEventListener("click", () => {
-    searchForm.classList.add("active"); 
-    cartItemsContainer.classList.remove("active"); 
-    navbar.classList.remove("active");
+    if(searchForm.style.right === '-120%' ||searchForm.style.right === '' ){
+        searchForm.style.right="7%";
+    }else{
+        searchForm.style.right="-120%";
+    }
+
+    cartItemsContainer.style.right="-100%";
+    navbar.style.right="-100%";
 });
 
 const cartIcon = document.querySelector(".cart-icon");
 const cartItemsContainer = document.querySelector(".cart-items-container");
 
 cartIcon.addEventListener("click", () => {
-    cartItemsContainer.classList.add("active"); 
-    searchForm.classList.remove("active"); 
-    navbar.classList.remove("active");
+    if(cartItemsContainer.style.right === '-100%' ||cartItemsContainer.style.right === '' ){
+        cartItemsContainer.style.right="0";
+    }else{
+        cartItemsContainer.style.right="-100%";
+    }
+
+    searchForm.style.right="-120%";
+    navbar.style.right="-100%";
 });
+
 const menuIcon = document.querySelector(".menu-icon");
 const navbar = document.querySelector(".navbar");
 
 menuIcon.addEventListener("click", () => {
-    navbar.classList.add("active");
-    searchForm.classList.remove("active");
-    cartItemsContainer.classList.remove("active");
+    if(navbar.style.right === '-100%' ||navbar.style.right === '' ){
+        navbar.style.right="0";
+    }else{
+        navbar.style.right="-100%";
+    }
+    searchForm.style.right="-120%";
+    cartItemsContainer.style.right="-100%";
 });
 
 window.onscroll = () =>{
+    searchForm.style.right="-120%";
+    cartItemsContainer.style.right="-100%";
     navbar.classList.remove("active");
-    searchForm.classList.remove("active");
-    cartItemsContainer.classList.remove("active");
+    navbar.style.right="-100%";
 }
